af314398bd8444e2322b645aa4e48039ecb4ae63
Rafael Delwart <rdelwart@ucsc.edu>
2025-07-15 15:11:20 -0700

    Adding changes in mixing in order to be able to control the speed of the mixing motors with PWM. (Frequency of PWM set low(500Hz) to protect the mosfet controlling the motors)  branch 'main' into LabBranch

--- Cycletron/src/MIXING.cpp
+++ Cycletron/src/MIXING.cpp
@@ -8,67 +8,91 @@
  * @date    25 Feb 2025 adapted for ESP 13 May 2025
  */
 
- #include <Arduino.h>
- #include "MIXING.h"
- 
+#include <Arduino.h>
+#include "MIXING.h"
 
-//  #define TESTING_MIXING
 
- // === CONFIG ===
- #define MIX1_GPIO 11
- #define MIX2_GPIO 12
- #define MIX3_GPIO 13  
- 
- // Define motor GPIOs in an array for batch operations
- static const uint8_t motorPins[] = {MIX1_GPIO, MIX2_GPIO, MIX3_GPIO};
- static const int NUM_MOTORS = sizeof(motorPins) / sizeof(motorPins[0]);
- 
- // === API IMPLEMENTATION ===
- 
- /**
-  * @brief Initializes all mixing motor GPIOs as OUTPUT and sets them LOW.
-  */
- void MIXING_Init() {
-   for (int i = 0; i < NUM_MOTORS; i++) {
-     pinMode(motorPins[i], OUTPUT);
-     digitalWrite(motorPins[i], LOW);  // Motors off by default
-   }
-   Serial.println("[MIXING] All motors initialized and set to OFF");
- }
- 
- /**
-  * @brief Turns ON the motor connected to the given GPIO pin.
-  * @param pin GPIO pin number
-  */
- void MIXING_Motor_OnPin(uint8_t pin) {
-   digitalWrite(pin, HIGH);
- }
- 
- /**
-  * @brief Turns OFF the motor connected to the given GPIO pin.
-  * @param pin GPIO pin number
-  */
- void MIXING_Motor_OffPin(uint8_t pin) {
-   digitalWrite(pin, LOW);
- }
- 
- /**
-  * @brief Turns ON all defined motors.
-  */
- void MIXING_AllMotors_On() {
-   for (int i = 0; i < NUM_MOTORS; i++) {
-     digitalWrite(motorPins[i], HIGH);
-   }
- }
- 
- /**
-  * @brief Turns OFF all defined motors.
-  */
- void MIXING_AllMotors_Off() {
-   for (int i = 0; i < NUM_MOTORS; i++) {
-     digitalWrite(motorPins[i], LOW);
-   }
- }
+// NOTE FOR VIKAS:
+// PWM control of mixing motors, the speed is controlled by the power value
+// 0 stops the motor, 255 is full speed. 
+#define mixingMotorPower 177 // Default motor power (0-255), can be set from backend
+
+
+
+
+
+#define mixingMotorPWMFrequency 500 // PWM frequency in Hz
+#define TESTING_MIXING
+
+// === CONFIG ===
+#define MIX1_GPIO 11
+#define MIX2_GPIO 12
+#define MIX3_GPIO 13
+
+// Define motor GPIOs and corresponding PWM channels
+static const uint8_t motorPins[] = {MIX1_GPIO, MIX2_GPIO, MIX3_GPIO};
+static const uint8_t motorChannels[] = {0, 1, 2}; // One channel per motor
+static const int NUM_MOTORS = sizeof(motorPins) / sizeof(motorPins[0]);
+
+// === API IMPLEMENTATION ===
+
+/**
+ * @brief Initializes all mixing motor GPIOs as PWM outputs (500Hz) and sets them LOW.
+ */
+void MIXING_Init() {
+  for (int i = 0; i < NUM_MOTORS; i++) {
+    pinMode(motorPins[i], OUTPUT);
+    ledcSetup(motorChannels[i], mixingMotorPWMFrequency, 8);           // 500 Hz, 8-bit resolution
+    ledcAttachPin(motorPins[i], motorChannels[i]); // Link pin to channel
+    ledcWrite(motorChannels[i], 0);                // Motors off by default
+  }
+  Serial.println("[MIXING] All motors initialized and set to OFF");
+}
+
+/**
+ * @brief Turns ON the motor connected to the given GPIO pin.
+ * @param pin GPIO pin number
+ */
+void MIXING_Motor_OnPin(uint8_t pin)
+{
+  for (int i = 0; i < NUM_MOTORS; i++) {
+    if (motorPins[i] == pin) {
+      ledcWrite(motorChannels[i], mixingMotorPower);
+      return;
+    }
+  }
+}
+
+/**
+ * @brief Turns OFF the motor connected to the given GPIO pin.
+ * @param pin GPIO pin number
+ */
+void MIXING_Motor_OffPin(uint8_t pin) {
+  for (int i = 0; i < NUM_MOTORS; i++) {
+    if (motorPins[i] == pin) {
+      ledcWrite(motorChannels[i], 0);
+      return;
+    }
+  }
+}
+
+/**
+ * @brief Turns ON all defined motors.
+ */
+void MIXING_AllMotors_On() {
+  for (int i = 0; i < NUM_MOTORS; i++) {
+    ledcWrite(motorChannels[i], mixingMotorPower);
+  }
+}
+
+/**
+ * @brief Turns OFF all defined motors.
+ */
+void MIXING_AllMotors_Off() {
+  for (int i = 0; i < NUM_MOTORS; i++) {
+    ledcWrite(motorChannels[i], 0);
+  }
+}
 
 // === TEST LOOP ===
 // This section is for testing the mixing motors.
